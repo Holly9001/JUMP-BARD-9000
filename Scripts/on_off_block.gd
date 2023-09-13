bbd78016d87ac0c255c8234da71a5ce15b0e9b36
rawsonb <110342737+rawsonb@users.noreply.github.com>
2023-09-13 10:29:29 -0400

    make on_off_block work with any children

--- Scripts/on_off_block.gd
+++ Scripts/on_off_block.gd
@@ -1,28 +1,31 @@
 extends Node3D
 
-@onready var block_collision = $StaticBody3D/CollisionShape3D
-
 const SWITCH_TIME = 1.5
 
+var child_collisions
+var children
 var time_until_switch = SWITCH_TIME
 
 func _ready():
-	var children = get_all_children(self)
-	for i in children:
-		print(i)
+	child_collisions = get_children_of_type(self, "CollisionShape3D")
+	children = get_children()
 
 func _process(delta):
-	print("test")
 	# this won't be happening here once the music works. it's just a timer to test
 	time_until_switch -= delta
 	# below code will check the music status singleton instead
 	if time_until_switch < 0:
 		time_until_switch = SWITCH_TIME
-		block_collision.disabled = !block_collision.disabled
-		self.visible = !self.visible
+		
+		for i in child_collisions:
+			i.disabled = !i.disabled
+		
+		for i in children:
+			i.visible = !i.visible
 
-func get_all_children(in_node,arr:=[]):
-	arr.push_back(in_node)
+func get_children_of_type(in_node, type, arr:=[]):
+	if in_node.get_class() == type:
+		arr.push_back(in_node)
 	for child in in_node.get_children():
-		arr = get_all_children(child,arr)
+		arr = get_children_of_type(child, type, arr)
 	return arr
