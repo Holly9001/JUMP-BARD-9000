4d7384cdc419989768932e06a4d8b69580df05d9
Holly9001 <johnandshelly621@gmail.com>
2023-09-14 16:26:06 -0400

    yeah

---
+++ Scripts/DebugUI.gd
@@ -0,0 +1,13 @@
+extends Control
+
+@onready var b_1_label :Label= $bass_1
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	
+	b_1_label.text = str('bass_1: ',MusicStates.bass_1)
