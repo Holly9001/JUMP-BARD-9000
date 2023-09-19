283591ee3243dcc9979605280732500f5255be69
Holly9001 <johnandshelly621@gmail.com>
2023-09-19 16:14:07 -0400

    corner boost + camera velocity fix

--- Scripts/PlayerSpringArm.gd
+++ Scripts/PlayerSpringArm.gd
@@ -11,8 +11,7 @@ extends SpringArm3D
 func _physics_process(_delta):
 	## iterate through an ['x','y'] list and only set those if the z freaks its shit here
 	
-	var movement_offset = player.movement_vector + Vector2(7 * player.velocity.x/6,0) + Vector2(0,1) ## vector offsets cam a bit so player is in bottom
-	
+	var movement_offset = player.movement_vector + Vector2(player.velocity.x/4,0) + Vector2(0,1) ## vector offsets cam a bit so player is in bottom
 	
 	location = Vector3(lerp(global_position.x,player_pos.x+movement_offset.x,0.02),lerp(global_position.y,player_pos.y + movement_offset.y,0.02),global_position.z)
 	
