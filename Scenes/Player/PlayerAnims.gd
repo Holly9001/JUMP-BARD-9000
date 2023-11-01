cb56d8de1808f6cadf435f429b378c7c65fcaedd
Holly9001 <johnandshelly621@gmail.com>
2023-11-01 18:53:53 -0400

    visual updates

---
+++ Scenes/Player/PlayerAnims.gd
@@ -0,0 +1,28 @@
+extends AnimatedSprite3D
+
+@onready var player :CharacterBody3D= get_parent()
+
+@onready var orig_scale : Vector3 = scale
+
+var player_speed : float
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	player_speed = snappedf(player.velocity.x,0.001)
+	
+	speed_scale = abs(player_speed)/3
+	
+	if player_speed == 0: animation = 'idle_1'
+	else:
+		scale.x = orig_scale.x * sign(player_speed)
+		animation = 'run_1'
+	
+	print('speed',speed_scale)
+	
+	print(player_speed)
